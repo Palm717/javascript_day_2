658c62bcc149633a259179ca8869953d1b8811c5
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-03-09 10:07:47 -0500

    Class Notes

---
+++ script.js
@@ -0,0 +1,78 @@
+var age = 17;
+var myName = 'RMP';
+
+// var operator = '+';
+
+// var a = 10;
+// var b = 15;
+
+if(age >= 25){
+        console.log(myName + ' is granted keys.');
+}else if(age=== 18){
+        console.log('old enough');
+}else {
+        console.log(myName +' is not old enough.');
+}
+
+//example for tonight
+
+
+
+
+
+
+
+
+
+
+
+// var color = 'orange'; //output orange
+//         var stringNum = '10'; //output 10
+//         var isAwesome = true; //output true
+//         var num = 11;
+
+//         var sameName = num === stringNum;
+//         var firstName = 'Robert';
+
+// var zero = 0; //falsey
+// var numOne= 50;
+// var empty = '';//falsey
+// var str = 'Some String';//truthy
+// var un = undefined;//falsey
+// var f = false;//falsey
+// var n = null;//falsey
+// var and = 100 && true;
+
+// var age =17;
+// var isOldEnough = age >= 18 || 'not';
+
+// console.log(isOldEnough);//compares two variables
+
+// console.log(and);//if both val are truthy it defaults to the last truthy input
+// console.log(zero || numOne);// && returns 0
+//                         // || returns 50 --continues until it finds a truthy value
+
+//         console.log(num, color);
+//         console.log(typeof isAwesome);//output boolean
+//         console.log(num + color);
+
+//         console.log(color + ' is the color of the sun')
+        
+//         console.log(5 + 5);//output 10
+//         console.log(5-5);//output 0
+//         console.log(5*5);//output 25
+
+//         console.log(num == stringNum);//checks the value 
+//         console.log(num === stringNum);//checks value and type
+//         console.log('not equal ', num !== stringNum);
+//         console.log('is ', sameName);//a variable attaching 2 outside variables for data
+//         console.log(firstName.includes('r'));//checks if a character(value) is within the text or other value
+
+//         //JavaScript is great at all kinds of math
+//         // multiple console logs within a value is possible
+//         // typeof is a base check of a value (string or num, etc)
+//         // console.log(color); -->
+
+//         // && represents --and 
+//         // || --or 
+//         // ! --not
